92f3727c77994c0c1788a03cc307882070a975ea
davifirman <115498002+Aliridho336@users.noreply.github.com>
2022-11-22 20:58:10 +0700

    new release

---
+++ app/src/main/java/com/example/a2018075_tugas6/DestinationActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2018075_tugas6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
